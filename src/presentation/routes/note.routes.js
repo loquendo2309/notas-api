0a10f9501eb1eb5cda6f545afd7e09ff223e1fff
loquendo2309 <lol2309lol@gmail.com>
2026-04-20 22:42:41 -0400

    incorporacion de JWT

--- src/presentation/routes/note.routes.js
+++ src/presentation/routes/note.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import NoteController from "../controllers/note.controller.js";
 import NoteService from "../../application/use-cases/note.service.js";
 import  upload  from "../middlewares/upload.middleware.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { roleMiddleware } from "../middlewares/role.middleware.js";
 
 //aqui definiremos que base de datos usar para las notas, en este caso MongoDB
 import NoteMongoRepository from "../../infrastructure/database/mongo/note.mongo.repository.js";
@@ -16,8 +18,13 @@ const noteController = new NoteController(noteService);
 
 const router = Router();
 
-//definir las rutas para las notas 
-router.post("/", upload.single('image'), noteController.createNote);
-router.get("/", noteController.getNotesByUserId);
+//definir las rutas para las notas  
+//comentado sin seguuridad
+//router.post("/", upload.single('image'), noteController.createNote);
+//router.get("/", noteController.getNotesByUserId);
+
+router.post("/",authMiddleware, upload.single('image'), noteController.createNote);
+router.get("/", authMiddleware, noteController.getNotesByUserId);
+
 
 export default router;
